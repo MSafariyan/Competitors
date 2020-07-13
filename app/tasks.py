225bbf6192986f497a705ea3825df2c15b4106d4
M. Safarian <ma.hiccup@gmail.com>
2020-07-13 06:39:17 +0430

    Add Crawler with POST request rq worker start the crawler in the background.

---
+++ app/tasks.py
@@ -0,0 +1,101 @@
+from bs4 import BeautifulSoup
+import lxml.html
+import re
+import requests
+from requests_html import HTMLSession
+import pandas as pd
+import time
+import os.path
+import mysql.connector
+import csv
+
+
+
+mydb = mysql.connector.connect(host='localhost', user='mahdi', password='',database='simplecrawl')
+
+cursor = mydb.cursor()
+
+
+def make_table_clean():
+    try:
+        print("============Cleaning Table============")
+        query = "TRUNCATE TABLE simplecrawl.books"
+        cursor.execute(query)
+        print("Status: Cleaning was successful")
+    except Exception as e:
+        print("==Cleaning database has been failed.==")
+        print(f"Error Exectopn: {e}")
+
+def fill_table():
+    try:
+        print("==========Updating Database===========")
+        if os.path.isfile('data/zabanshop.csv') == True:
+            rows = csv.reader(open('data/zabanshop.csv'))
+            for row in rows:
+
+                cursor.execute('INSERT INTO books (name,price,oldprice,specialprice) VALUES(%s,%s,%s,%s)', row)
+        print("Status: Updating was succesful")
+    except Exception as e:
+        print("Status: Updating failed")
+        print(f"Error exception: {e}")
+
+def create_csv(records):
+    try:
+        print("=============Creating CSV=============")
+        if os.path.isfile('data/zabanshop.csv') == True:
+            os.remove("data/zabanshop.csv")
+        df = pd.DataFrame(records, columns=['name','price','oldprice','specialprice'])
+        df.to_csv('data/zabanshop.csv', index=False, encoding='utf-8')
+        print("CSV file is ready")
+    except Exception as e:
+        print("Status: Creating failed")
+        print(f"Error Exception: {e}")
+
+
+def zabanshop():
+    try:
+        print("============Try to connect============")
+        http_proxy  = "http://127.0.0.1:9150"
+        proxy = {'http': http_proxy}
+        html = requests.get('https://zaban.shop/', proxies=proxy)
+        print("fetching: zabanshop")
+        root = lxml.html.fromstring(html.text)
+        hrefs = root.xpath("//li[contains(@class, 'has-sub-category')]/ul/li/a")
+        records = []
+        count = 1
+        len_hrefs = len(hrefs)
+        print("Links = ",len_hrefs)
+        for href in hrefs:
+            url = href.attrib['href'] + '/?pageitems=120' + '&only_available=1'
+            if re.search('\d*_\d*_\d*', url):
+                print(f"[{count}/" + str(len_hrefs) + "]")
+                response = requests.get(url, proxies=proxy).text
+                soup = BeautifulSoup(response, 'html.parser')
+                items = soup.find_all('div', attrs={'class':'product-item'})
+                count = count + 1
+                for item in items:
+                    if item.find("a", attrs={'class':'product-name'}) == None:
+                        pass
+                    elif item.find('span', {'class':{'productPrice'}}) == None:
+                        name = item.find("a", {'class':'product-name'}).text.strip()
+                        price = "None"
+                        specialprice= item.find('span', {'class':'productSpecialPrice'}).text
+                        oldprice = item.find('span', {'class':'productOldPrice'}).text
+                    else:
+                        name = item.find("a", {'class':'product-name'}).text.strip()
+                        price = item.find('span',{'class':{'productPrice'}}).text
+                        specialprice= "None"
+                        oldprice = "None"
+                    records.append((name, price, specialprice, oldprice))
+        create_csv(records)
+        make_table_clean()
+        fill_table()
+        mydb.commit()
+        cursor.close()
+        print("======================================")
+        print("Finished")
+        print(f"there are just {count} valid links")
+    except Exception as e:
+        print("Mission failed")
+        print(e)
+        print("======================================")
--- app/views.py
+++ app/views.py
@@ -1,9 +1,18 @@
 from app import app
 from app import r
 from app import q
+from app.tasks import zabanshop
 
-from flask import render_template, request
+from flask import render_template, request,jsonify
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    return "hello world"
+
+    jobs = q.jobs
+    if request.method == 'POST':
+        task = q.enqueue(zabanshop)
+        jobs = q.jobs
+        message = q
+        return render_template('home.html', message=jobs)
+    else:
+        return render_template('home.html')
