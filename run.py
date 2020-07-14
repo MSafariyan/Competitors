0ef82316b869ddf0244ee5a93cc5cf6544c7adb4
M. Safarian <ma.hiccup@gmail.com>
2020-07-15 01:36:04 +0430

    Add rq monitor to project

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from app import app
+import rq_dashboard
+
+app.config.from_object(rq_dashboard.default_settings)
+app.register_blueprint(rq_dashboard.blueprint, url_prefix="/rq")
 
 if __name__ == '__main__':
     app.run()
